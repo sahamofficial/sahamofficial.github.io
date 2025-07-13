288b43acdc096e61d9b7d96f5eea6214f04f4e42
sahamofficial <sahamaliofficial2gmail.com>
2025-07-13 11:49:58 +0530

    code previews

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,14 +12,18 @@
   /**
    * Header toggle
    */
-  const headerToggleBtn = document.querySelector('.header-toggle');
+const headerToggleBtn = document.querySelector('.header-toggle');
 
+if (headerToggleBtn) {
   function headerToggle() {
     document.querySelector('#header').classList.toggle('header-show');
     headerToggleBtn.classList.toggle('bi-list');
     headerToggleBtn.classList.toggle('bi-x');
   }
+
   headerToggleBtn.addEventListener('click', headerToggle);
+}
+
 
   /**
    * Hide mobile nav on same-page/hash links
